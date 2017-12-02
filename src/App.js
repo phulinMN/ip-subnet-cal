67a92da73e82f5e7ac224485ee9c9020d16d89f2
phulinMN <pongpaiMN@gmil.com>
2017-12-02 23:12:23 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { subnet, classSubnet, IpToBinary, NetworkAdd, usableLength, broadcast, ResultHost,ResultUsable, WildcardMask, BinarySubnet, classIp, IpType, Short, BinaryID, HexID, IpToDecimal, AllPossibleNetAdd, IsIpv4} from './utils/helper'
-import { Button, Icon, Radio, Input, Select, Grid, Form, Table } from 'semantic-ui-react'
+import { Button, Icon, Radio, Input, Select, Grid, Form, Table, Segment } from 'semantic-ui-react'
 
 const subnetToSelectArray = (subnetArray) => {
   return subnetArray.map((subnet, n) => {
@@ -54,61 +54,84 @@ class App extends Component {
           <div className="row">
             {/* Header */}
             <div className="page-header">
-              <label id="text-header">IP Subnet Calculator</label>
-              <Grid>
-                <Grid.Row centered columns={6}>
-                  <Grid.Column>Network Class</Grid.Column>
+              {/* <label id="text-header">IP Subnet Calculator</label> */}
+              <Grid container>
+                <Grid.Row centered columns={2}>
                   <Grid.Column>
-                  <Form>
-                    <Form.Field>
-                      <Radio 
-                        label='Any'
-                        name='radio'
-                        value='Any'
-                        checked={this.state.type === 'Any'}
-                        onChange={this.handleChangeRadio}
-                      />
-                      <Radio
-                        label='A'
-                        name='radio'
-                        value='A'
-                        checked={this.state.type === 'A'}
-                        onChange={this.handleChangeRadio}
-                      />
-                      <Radio
-                        label='B'
-                        name='radio'
-                        value='B'
-                        checked={this.state.type === 'B'}
-                        onChange={this.handleChangeRadio}
-                      />
-                      <Radio
-                        label='C'
-                        name='radio'
-                        value='C'
-                        checked={this.state.type === 'C'}
-                        onChange={this.handleChangeRadio}
-                      />
-                    </Form.Field>
-                  </Form>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row centered columns={6}>
-                  <Grid.Column>Subnet</Grid.Column>
-                  <Grid.Column>
-                    <Select placeholder="Select Subnet" options={subnetToSelectArray(this.state.subnetall)} onChange={this.handleChangeSelect} />
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row centered columns={6}>
-                  <Grid.Column>IP Address</Grid.Column>
-                  <Grid.Column>
-                    <Input placeholder="IPV4 example: 158.104.14.15" onChange={this.handleChangeInput} type="text" name="ip-address"/>
-                    <Button primary disabled={!IsIpv4(this.state.ip)} onClick={this.handleClick} animated>
-                      <Button.Content visible>Calculate</Button.Content>
-                      <Button.Content hidden>
-                        <Icon name='right arrow' />
-                      </Button.Content>
-                    </Button>
+                    <Table>
+                      <Table.Header>
+                        <Table.Row celled>
+                          <Table.HeaderCell id="text-header" textAlign='center' colSpan='2'>IP Subnet Calculator</Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Body id="table-header">
+                        <Table.Row>
+                          <Table.Cell>Network Class</Table.Cell>
+                          <Table.Cell>
+                            <Form>
+                              <Form.Field>
+                                <Table.Cell>
+                                  <Radio id="check-box"
+                                    label='Any'
+                                    name='radio'
+                                    value='Any'
+                                    checked={this.state.type === 'Any'}
+                                    onChange={this.handleChangeRadio}
+                                  />
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <Radio id="check-box"
+                                    label='A'
+                                    name='radio'
+                                    value='A'
+                                    checked={this.state.type === 'A'}
+                                    onChange={this.handleChangeRadio}
+                                  />
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <Radio id="check-box"
+                                    label='B'
+                                    name='radio'
+                                    value='B'
+                                    checked={this.state.type === 'B'}
+                                    onChange={this.handleChangeRadio}
+                                  />  
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <Radio id="check-box"
+                                    label='C'
+                                    name='radio'
+                                    value='C'
+                                    checked={this.state.type === 'C'}
+                                    onChange={this.handleChangeRadio}
+                                  />
+                                </Table.Cell>
+                              </Form.Field>
+                            </Form>
+                          </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                          <Table.Cell>Subnet</Table.Cell>
+                          <Table.Cell>
+                            <Select id="text-select" placeholder="Select Subnet" options={subnetToSelectArray(this.state.subnetall)} onChange={this.handleChangeSelect} />
+                          </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                          <Table.Cell>IP Address</Table.Cell>
+                          <Table.Cell>
+                            <Input id="text-input" placeholder="IPV4 example: 158.104.14.15" onChange={this.handleChangeInput} type="text" name="ip-address"/>
+                            <Button id="btn-cal" disabled={!IsIpv4(this.state.ip)} onClick={this.handleClick} animated>
+                              <Button.Content visible icon='calculator'>
+                                <Icon name='calculator' />
+                              </Button.Content>
+                              <Button.Content hidden>
+                                <Icon name='right arrow' />
+                              </Button.Content>
+                            </Button>
+                          </Table.Cell>
+                        </Table.Row>
+                      </Table.Body>
+                    </Table>
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
@@ -119,17 +142,17 @@ class App extends Component {
             {
               this.state.checked &&
               <div>
-                 <Grid>
-                  <Grid.Row centered columns={3}>
+                 <Grid container>
+                  <Grid.Row centered columns={2}>
                     <Grid.Column>
                       <Table celled selectable>
                         <Table.Header>
                           <Table.Row>
-                            <Table.HeaderCell colSpan='2'>IP Subnet Calculator</Table.HeaderCell>
+                            <Table.HeaderCell id="text-header" textAlign='center' colSpan='2'>RESULT</Table.HeaderCell>
                           </Table.Row>
                         </Table.Header>
 
-                        <Table.Body>
+                        <Table.Body id="table-result">
                           <Table.Row>
                             <Table.Cell>IP Address</Table.Cell>
                             <Table.Cell>{ this.state.ipUse }</Table.Cell>
@@ -206,23 +229,28 @@ class App extends Component {
             {/* All Possible table */}
             {
               this.state.checked &&
-              <Grid>
-                <Grid.Row centered columns={3}>
+              <Grid container>
+                <Grid.Row centered columns={2}>
                   <Grid.Column>
-                    <Table  celled selectable> 
+                    <Table celled selectable> 
                       <Table.Header>
                         <Table.Row>
-                          <Table.HeaderCell>Network Address</Table.HeaderCell>
-                          <Table.HeaderCell>Usable Host LengTable.Cell</Table.HeaderCell>
-                          <Table.HeaderCell>Broadcast Address</Table.HeaderCell>
+                          <Table.HeaderCell id="text-header" textAlign="center" colSpan='3'>All Possible Network</Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Header>
+                        <Table.Row>
+                          <Table.HeaderCell id="header-allp" textAlign="center">Network Address</Table.HeaderCell>
+                          <Table.HeaderCell id="header-allp" textAlign="center">Usable Host LengTable.Cell</Table.HeaderCell>
+                          <Table.HeaderCell id="header-allp" textAlign="center">Broadcast Address</Table.HeaderCell>
                         </Table.Row>
                       </Table.Header>
 
-                      <Table.Body>
+                      <Table.Body id="table-all">
                         { AllPossibleNetAdd(this.state.ipUse, this.state.n).map((net, i) =>
                         <Table.Row>
                           <Table.Cell>{ net }</Table.Cell>
-                          <Table.Cell>{ usableLength(net, this.state.n) }</Table.Cell>
+                          <Table.Cell textAlign='center'>{ usableLength(net, this.state.n) }</Table.Cell>
                           <Table.Cell>{ broadcast(net, this.state.n) }</Table.Cell>
                         </Table.Row>
                         )}
